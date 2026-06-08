4fbbefee0e096087625fb54d505f1fe6a0656b6d
jawadalzein59-rgb <jawadalzein598@gmail.com>
2026-06-08 03:00:35 +0300

    Add files via upload
    fix: Z-factor corrected + complete working notebook

--- fluid.py
+++ fluid.py
@@ -9,34 +9,31 @@ class Fluid:
         self.xa = xa
         self.xy = xy
         self.T = T
-        self.R = 8.314462618  # J/(mol·K)
-        self.P_std = 1.01325   # atm
-        self.T_std = 293.15    # K
-
-        # Load viscosity data
+        self.R = 8.314462618
+        self.P_std = 1.01325
+        self.T_std = 293.15
+        self.P_pc = 46.0
+        self.T_pc = 190.0
         df = pd.read_csv('interp_data.csv')
         self.mu_interp = LinearInterpolator(df.iloc[:, 0].tolist(), df.iloc[:, 1].tolist())
 
     def z(self, P: float) -> float:
-        # GERG-91 модифицированный (универсальная аппроксимация для природного газа)
-        # Если у тебя есть точная формула из hw2.ipynb, замени тело этой функции
-        Pc = 46.0  # атм
-        Tc = 190.0 # K
-        Pr = P / Pc
-        Tr = self.T / Tc
-        Z = 1.0 - 3.52 * Pr / (Tr**2.2) + 0.274 * (Pr**2) / (Tr**5.3)
-        return max(Z, 0.1)
+        Pr = P / self.P_pc
+        Tr = self.T / self.T_pc
+        if Tr > 1.0 and Pr < 15:
+            Z = 1 - 3.52 * Pr / (10 ** (0.9813 * Tr)) + 0.274 * (Pr ** 2) / (10 ** (0.8157 * Tr))
+        else:
+            Z = 1 - 3.52 * Pr / (Tr ** 2.2) + 0.274 * (Pr ** 2) / (Tr ** 5.3)
+        return max(0.7, min(2.0, Z))
 
     def ro(self, P: float) -> float:
         Z = self.z(P)
         P_pa = P * 101325.0
-        # rho = P*M / (Z*R*T), M в г/моль -> делим на 1000
         rho = (P_pa * (self.M / 1000.0)) / (Z * self.R * self.T)
         return rho
 
     def bg(self, P: float) -> float:
         Z = self.z(P)
-        # B_g = Z*T*P_std / (P*T_std) при Z_std ~ 1
         return (Z * self.T * self.P_std) / (P * self.T_std)
 
     def mu(self, P: float) -> float:
